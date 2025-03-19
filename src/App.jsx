e9404b903b9a984723e7cd26d739c57e0f1d1885
redfyel <lianis.ee7@gmail.com>
2025-03-19 23:19:10 +0530

    yay, backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const API_URL = "https://incorporating-data.vercel.app/api";
+const API_URL = "https://incorporating-data-demo.onrender.com";
 
 const steps = [
   { 
